b936758659f79afeedcff0eb744ad089c2e4946a
IATec - Rodrigo Leme <rodrigo.leme@iatecbr-mac034.local>
2022-05-12 20:33:10 -0300

    First Commit / Label footer

---
+++ on_boarding_ui/android/app/src/main/kotlin/com/example/on_boarding_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.on_boarding_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ on_boarding_ui/lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        color: Colors.black,
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Placeholder(
+                fallbackWidth: 20,
+                fallbackHeight: 150,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Placeholder(
+                fallbackHeight: 50,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Placeholder(
+                fallbackHeight: 50,
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Placeholder(
+                fallbackHeight: 50,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Placeholder(
+                fallbackHeight: 50,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              // Text(
+              //   'Sign In',
+              //   style: TextStyle(color: Colors.white, fontSize: 16),
+              // ),
+              RichText(
+                text: const TextSpan(
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                  children: [
+                    TextSpan(text: 'Already have an account? '),
+                    TextSpan(
+                        text: 'Sign In',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.bold))
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
